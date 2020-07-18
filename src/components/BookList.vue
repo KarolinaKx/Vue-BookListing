d1911c033bada160ee41d43d58900db81dbaf47b
Karolina Kustron <karolina@cedita.com>
2020-07-18 19:55:09 +0100

    stage 2

---
+++ src/components/BookList.vue
@@ -0,0 +1,24 @@
+<template>
+
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+
+</template>
+
+<script>
+  export default {
+  name: 'BookList',
+  data() {
+    return {
+      msg: 'All Books',
+    };
+  },
+};
+</script>
+
+<style>
+  h1, h2 {
+  font-weight: normal;
+  }
+</style>
